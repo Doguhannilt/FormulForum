d81fdc43bca4497b17014c90751794892e4886a5
KaanCL <Kaancerkez34@outlook.com>
2024-05-22 22:46:44 +0300

    frontend eklendi

---
+++ Frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/navbar'
+function App() {
+  return (
+<div className='body'>
+<Navbar/>
+<hr/>  
+</div>
+  );
+}
+
+export default App;
---
+++ Frontend/my-app/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Navbar(){
+return(
+    <div>
+    <a target="_blank" href="login.html" class="button">Login</a>
+    <img className="logo" src="https://cdn.wmaraci.com/nedir/forum.png"></img>
+    
+    </div>
+)
+
+}
+export default Navbar;
