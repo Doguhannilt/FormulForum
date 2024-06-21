4a2f4bfe822d7f4e128ddd1547655f68d4dcde9f
KaanCL <Kaancerkez34@outlook.com>
2024-06-21 21:18:28 +0300

    id değeri kaldırıldı

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
       name:{type:String,required:true},
-      id:{type:String,required:true},
+      //id:{type:String,required:true},
       picture:{type:String},
       email: { type: String, required: true },
 })
