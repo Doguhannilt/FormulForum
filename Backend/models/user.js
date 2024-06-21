ffed0ebc33cb43e93670134f3e6d3763cf17f075
KaanCL <Kaancerkez34@outlook.com>
2024-06-21 21:21:30 +0300

    uniName değeri eklendi

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
       //id:{type:String,required:true},
       picture:{type:String},
       email: { type: String, required: true },
+      uniName:{ type: String, default:null }
 })
 const User = mongoose.model("User",UserSchema)
 module.exports = User;
