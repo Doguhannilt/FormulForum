68c9fe2489b2533047a805e820cd5e785f2c8a84
KaanCL <Kaancerkez34@outlook.com>
2024-06-27 21:57:12 +0300

    import articleCounter function

--- Backend/controllers/postArticles.js
+++ Backend/controllers/postArticles.js
@@ -1,10 +1,10 @@
 const Post = require("../models/availablePost")
-
+const articleCounter = require("./articleCounter");
 // Get all informations given by client-side and save it
 const postArticles = async (req, res) => {
     try {
         const posts = new Post({
-           //  id: req.body.id,
+            userId:req.body.userId,
             title: req.body.title,
             text: req.body.text,
             date: req.body.date,
@@ -13,6 +13,12 @@ const postArticles = async (req, res) => {
             username: req.body.username,
         });
         await posts.save();
+        try {
+            await articleCounter(posts.userId);
+        } catch (err) {
+            console.error('Error updating article count:', err);
+            return res.status(500).json({ error: 'Failed to update article count' });
+        }
         res.json(posts);
     } catch (err) {
         res.status(500).json({error:err.message});
