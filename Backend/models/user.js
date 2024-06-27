da3fd2dcc4ef18c891018472791e3964f2914fae
KaanCL <Kaancerkez34@outlook.com>
2024-06-27 22:00:30 +0300

    add uniName , departmentName , grade , articleCount

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new db.Schema({
       name:{type:String,required:true},
-      //id:{type:String,required:true},
       picture:{type:String},
       email: { type: String, required: true },
-      uniName:{ type: String, default:null }
+      uniName:{type:String , default:null},
+      departmentName:{type:String,default:null},
+      grade:{type:Number , default:null},
+      articleCount:{type:Number,default:null},    
 })
 const User = mongoose.model("User",UserSchema)
 module.exports = User;
