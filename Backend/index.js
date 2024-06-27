6aa1c625fa7e4bf9321f54ca0ef541b638da193e
KaanCL <Kaancerkez34@outlook.com>
2024-06-27 21:57:35 +0300

    add userRouter

--- Backend/index.js
+++ Backend/index.js
@@ -3,13 +3,15 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-const cors = require("cors"
+const cors = require("cors")
 const { expressjwt: jwt } = require("express-jwt");
 const jwks = require("jwks-rsa")
 
 // Routers
-const postRouter = require("./routers/posts")
+const postRouters = require("./routers/posts")
 const signupRouter = require("./routers/signup")
+const userRouter = require("./routers/users")
+
 
 // Ports and Express
 const auth = require("./middlewares/auth")
@@ -29,7 +31,7 @@ app.use("/protected",signupRouter)
 console.log("signupRouter has been working on protected URL")
 app.use("/post",postRouter);
 console.log("postRouter has been working on postRouter URL")
-
+app.use("/user",userRouter);
 // MONGODB 
 mongoose
 .connect(process.env.CONNECTION_URL,
